8b194756d9ca76d822759d51f5a6b265462d0a18
Kemal Sebzeci <kemal.sebzeci.12@gmail.com>
2026-02-03 02:33:12 +0300

    add: variables example

---
+++ 02-variables.sh
@@ -0,0 +1,10 @@
+'#!/bin/bash'
+
+# Variable Definition (NO SPACE after the equals sign)
+name="Futhark"
+os="Fedora Linux"
+version=43
+
+# Using the Variable (called with the $ sign)
+echo "Hello, I'm $name."
+echo "My OS is: $os version $version"
